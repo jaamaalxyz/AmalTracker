ab2ee7901ee1e7efe06067e02701d6809b5f1aba
Md. Jamal Uddin <jamal.pb95@gmail.com>
2022-04-14 19:56:43 +0600

    add Home Icon on Bottom Tab Navigation

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -1,5 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
+import { Text } from 'react-native';
+import { HomeIcon } from '../components/Icons';
+import { theme } from '../theme';
 import Analytics from './Analytics.screen';
 import History from './History.screen';
 import Home from './Home.screen';
@@ -8,10 +11,42 @@ const BottomTabs = createBottomTabNavigator();
 
 const BottomTabsNavigator: React.FC = () => {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name="Home" component={Home} />
-      <BottomTabs.Screen name="History" component={History} />
-      <BottomTabs.Screen name="Analytics" component={Analytics} />
+    <BottomTabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: theme.colorBlue,
+        tabBarInactiveTintColor: theme.colorGrey,
+        tabBarShowLabel: false,
+        tabBarIcon: ({ color, size }) => {
+          if (route.name === 'Home') {
+            return <HomeIcon color={color} size={size} />;
+          }
+
+          if (route.name === 'History') {
+            return <Text>History</Text>;
+          }
+
+          if (route.name === 'Analytics') {
+            return <Text>Analytics</Text>;
+          }
+
+          return null;
+        },
+      })}>
+      <BottomTabs.Screen
+        name="Home"
+        component={Home}
+        options={{ title: "Today's Mood" }}
+      />
+      <BottomTabs.Screen
+        name="History"
+        component={History}
+        options={{ title: 'Past Mood' }}
+      />
+      <BottomTabs.Screen
+        name="Analytics"
+        component={Analytics}
+        options={{ title: 'Fancy Charts' }}
+      />
     </BottomTabs.Navigator>
   );
 };
--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,8 @@ export interface MoodOptionTypeWithTimestamp {
   mood: MoodOptionType;
   timestamp: number;
 }
+
+export interface IconProps {
+  color?: string;
+  size?: number;
+}
