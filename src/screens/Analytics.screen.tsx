936bc1b95c386331906310e01efef440a3cfaad3
Md. Jamal Uddin <jamal.pb95@gmail.com>
2022-04-15 23:44:28 +0600

    add Pie Chart using Victory Pie

--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,10 +1,17 @@
+import { groupBy } from 'lodash';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { VictoryPie } from 'victory-native';
+import { useAppContext } from '../App.provider';
 
 const Analytics: React.FC = () => {
+  const appContext = useAppContext();
+  const data = Object.entries(groupBy(appContext.moodList, 'mood.emoji')).map(
+    ([key, value]) => ({ x: key, y: value.length }),
+  );
   return (
     <View style={styles.container}>
-      <Text>Analytics</Text>
+      <VictoryPie data={data} />
     </View>
   );
 };
